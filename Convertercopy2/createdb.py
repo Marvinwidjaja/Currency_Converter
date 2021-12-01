f2f82665860cf3db4666daeb27dfc192edca2826
Marvinwidjaja <50818608+Marvinwidjaja@users.noreply.github.com>
2021-12-02 00:04:53 +0300

    Delete Convertercopy2 directory

--- Convertercopy2/createdb.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('/Users/lol/Downloads/Convertercopy2/database.db')
-
-conn.execute('CREATE TABLE students (amount INT, from_curr TEXT, to_curr TEXT, amount_final INT)')
-
-conn.close()
